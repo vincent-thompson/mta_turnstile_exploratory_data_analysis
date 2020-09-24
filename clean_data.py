6d8c97d4191889372e4877df0c07fb7f00614718
Nick Wilders <nawilders@gmail.com>
2020-09-24 19:29:32 -0400

    updated clean_data function

--- clean_data.py
+++ clean_data.py
@@ -4,60 +4,42 @@ def clean_data(df):
     import datetime as dt
     import numpy as np
    
-    # rename columns to get rid of the funny 'EXITS     ' issue
+    # standardizes column name and spacing for easy calling in code
     df.columns = ['C/A','UNIT','SCP','STATION','LINENAME',\
                   'DIVISION','DATE','TIME','DESC','ENTRIES','EXITS']
     
-    # remove index 180052 (project technology error)
-    df.drop(df.index[180052])
+    ### TIMEDATE conversion / rounding
     
-    # convert to datetime/make turnstile column
+    # converts the time to datetime function to make it easier to work with this data, and consolidated turnstile information
     df['converted_time'] = pd.to_datetime(df['DATE']+' '+df['TIME'])
     df['turnstiles'] = df['C/A'] + '-' + df['UNIT'] + '-' + df['SCP'] + '-' + df['STATION']
-    
-    # sort by date and location
     df_sorted = df.sort_values(['turnstiles', 'converted_time'])
     
-    # group by turnstile so we can get entry/exit differences
+    # rounds timeframe to nearest hour 
+    df_sorted['time_round']=pd.to_datetime(df['TIME'], format='%H:%M:%S')
+    df_sorted['time_round']=df_sorted['time_round'].dt.round('H').dt.hour
+    
+    ### time-specific ENTRIES / EXITS and total TURNSTILE_TURNS columns 
+    
+    # creats a new column to represent number of turns per time period, as opposed to cumulative over full collection period
     turnstile_df = df_sorted.groupby('turnstiles')
     df_sorted['entries_diff'] = turnstile_df['ENTRIES'].diff()
     df_sorted['exits_diff'] = turnstile_df['EXITS'].diff()
     
-    '''
-    # calculates IQR for entries_diff
-    Q3 = df_sorted['entries_diff'].quantile(0.75) 
-    Q1 = df_sorted['entries_diff'].quantile(0.25)
-    IQR = Q3 - Q1
-    
-    # calculates IQR range using outliers 
-    IQR_range = (Q1 - 1.5 * IQR, Q3 + 1.5 * IQR)
-    
-    # removes values outside of lower and upper bounds
-    df_sorted = df_sorted[df_sorted['entries_diff'].between(0, IQR_range[1])]
-    
-    # repeats process for exits_diff
-    Q3_2 = df_sorted['exits_diff'].quantile(0.75) 
-    Q1_2 = df_sorted['exits_diff'].quantile(0.25)
-    IQR_2 = Q3_2 - Q1_2
-    IQR_range_2 = (Q1_2 - 1.5 * IQR, Q3_2 + 1.5 * IQR)
-    df_sorted = df_sorted[df_sorted['exits_diff'].between(0, IQR_range_2[1])]
-    '''
-    
-    # removes negative values (ONLY NEEDED if no outliers removed)
+    # removes negative values (connected to audits and technological interferences)
     df_sorted = df_sorted[df_sorted['entries_diff'].between(0, np.inf)]
     df_sorted = df_sorted[df_sorted['exits_diff'].between(0, np.inf)]
     
-    # round to nearest hour 
-    df_sorted['time_round']=pd.to_datetime(df['TIME'], format='%H:%M:%S')
-    df_sorted['time_round']=df_sorted['time_round'].dt.round('H').dt.hour
-    
-    # created new column turnstile_turns with total turnstile interactions per turnstile
-    df_sorted['total_turns'] = df_sorted.entries_diff + df_sorted.exits_diff
-
     # replaces NaN values with mean for entries_diff and exits_diff
     df_sorted.entries_diff = df_sorted.entries_diff.fillna(df_sorted.entries_diff.mean())
     df_sorted.exits_diff = df_sorted.exits_diff.fillna(df_sorted.exits_diff.mean())
+    
+    # created new column turnstile_turns with total turnstile interactions per turnstile
+    df_sorted['total_turns'] = df_sorted.entries_diff + df_sorted.exits_diff
+    
 
+    ### DAY_OF_WEEK column
+    
     # provides column day_of_week that designates the day of the week 
     def get_weekday(x):
         weekday_key = {0:'Mon', 1:'Tue', 2:'Wed', 3:'Thu', 4:'Fri', 5:'Sat', 6:'Sun' }
@@ -67,7 +49,9 @@ def clean_data(df):
     #Create new column to differentiate stations serving different subway lines but with identical names
     df_sorted['station_unique'] = df_sorted['STATION'] + '-' + df_sorted['LINENAME']
     
-<<<<<<< HEAD
+    
+    ### Merges LATITUDE / LONGITUDE data
+    
     # merges with latitude and longitude data, providing three new columns - lat, lng and location_string (string w coordinates)
     github_df = pd.read_csv("https://raw.githubusercontent.com/dirtylittledirtbike/mta_data/master/location_by_remote_unit.csv")
     new_df = pd.merge(df_sorted, github_df, on='UNIT', how='left') 
@@ -98,6 +82,3 @@ def clean_data(df):
                   ['lat', 'lng', 'location_string']] = 40.6895, -74.1745, '40.6895, -74.1745'
     
     return new_df
-=======
-    return df_sorted
->>>>>>> d78b28a879df86bf66af0139a3dccea25ecd0378
